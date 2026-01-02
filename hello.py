ce6ff65d6c8a1cd0709dfaa9191bc6a267dd6c29
Jumahvirginia <Jumahvirginia@gmail.com>
2026-01-02 22:22:24 +0300

    Day 2:Methods - capitalize(),strip()

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-print("hello,world")
-print("This is cs50P:Programming in Python")
-input("Hello,what is your name?")
-print("Nice to meet you,")
+
+# print("hello,world")
+#print("This is cs50P:Programming in Python")
+#input("Hello,what is your name?")
+#print("Nice to meet you,")
+
+name = input("Hello,what is your name? ")
+
+#Remove whitespace from beginning & end of string
+name = name.strip()
+
+#Capitalizes the name
+#It only Capitalizes only the first letter of the string
+name = name.capitalize()
+
+print("Nice to meet you,",name)
