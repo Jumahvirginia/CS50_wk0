d29760ddb09f0f06fb857830d87fd48ae528ced0
Jumahvirginia <Jumahvirginia@gmail.com>
2026-01-03 22:41:09 +0300

    day3: methods-title(),split()

--- hello.py
+++ hello.py
@@ -13,4 +13,22 @@ name = name.strip()
 #It only Capitalizes only the first letter of the string
 name = name.capitalize()
 
+#Capitalize the first letter of each word in the string
+name = name.title()
+
+#A simpler way to do all of the above in one line
+name = input("Hello,what is your name? ").strip().title()
+
+#prints the final output
 print("Nice to meet you,",name)
+
+#split() method splits a string into a list where each word is a list item
+first, last = name.split(" ")
+
+print("Your first name is",first)
+print("Your last name is",last)
+print("Nice to meet you,", name)
+
+myfriends = "john, jane, mary, bob"
+friends = myfriends.split(",")
+print("These are my friends:", friends)
